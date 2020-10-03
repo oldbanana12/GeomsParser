8c502c5609a860146695802abf8320d798437438
oldbanana12 <69821086+oldbanana12@users.noreply.github.com>
2020-10-03 12:55:16 +0100

    Update ArtifactBuild.cmd

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-powershell Compress-7Zip "x64\Release" -ArchiveFileName "SampleX64.zip" -Format Zip
+powershell Compress-7Zip "bin\x64\Release" -ArchiveFileName "GeomsParserX64.zip" -Format Zip
 
 :exit
 popd
